d5a381a305eaf0888624dbad69ac69c875bb0ba0
ALIFOE <alifoebaudoin228@gmail.com>
2025-12-19 11:03:25 +0000

    repos de lien

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/ONG_MDH/',
+  base: process.env.NODE_ENV === 'production' ? '/ONG_MDH/' : '/',
   plugins: [vue()],
   server: {
     port: 5173,
